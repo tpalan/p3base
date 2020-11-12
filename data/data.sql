d5027aa8b3312e200b865a37dd5fc71ea1824976
Tom Palan <thomas@palan.at>
2020-11-12 16:19:05 +0100

    fix austrian address format, remove state

--- data/data.sql
+++ data/data.sql
@@ -1,2 +1,2 @@
-UPDATE res_country SET address_format=E'%(street)s\n%(street2)s\n%(zip)s %(city)s %(state_code)s' WHERE code = 'AT';
+UPDATE res_country SET address_format=E'%(street)s\n%(street2)s\n%(zip)s %(city)s' WHERE code = 'AT';
 UPDATE res_lang SET date_format = '%d.%m.%Y' WHERE code = 'de_DE';
